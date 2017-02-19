ca3f133c1f0b0cddfee0b1ccb95fea7344d865b3
David Solberg <dwsolberg@whittencreative.com>
2017-02-19 15:00:15 -0600

    Initial Commit

---
+++ Convenience/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Convenience
+//
+//  Created by David Solberg on 2/19/17.
+//  Copyright © 2017 David Solberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
